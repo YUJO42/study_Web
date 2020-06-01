8455d99d1c9dd7c1d6a4e4b8b73e3c11253ba719
yujo <yujo@student.42seoul.kr>
2020-06-01 19:46:56 +0900

    cloning lecture files, and section1 - 01challange done

---
+++ Complete_Javascript_Udemy/2-JS-basics/starter/script.js
@@ -0,0 +1,11 @@
+var massYujo = 75; //kg
+var heightYujo = 1.78; // meters
+
+var massYixing = 60; // kg
+var heightYixing = 1.71; // meters
+
+var BMIyujo = massYujo / (heightYujo * heightYujo);
+var BMIyixing = massYixing / (heightYixing * heightYixing);
+
+console.log("yujo BMI : " + BMIyujo);
+console.log("yuxing BMI : " + BMIyixing);
