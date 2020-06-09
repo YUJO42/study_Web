2e6cc93a478b27c9eece88380dcb51ce508c4333
yujo <yujo@student.42seoul.kr>
2020-06-10 00:26:19 +0900

    make to-do list

--- JS_cloning_chrome_app/src/greeting.js
+++ JS_cloning_chrome_app/src/greeting.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
+
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
@@ -23,6 +24,7 @@ function askForName() {
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
